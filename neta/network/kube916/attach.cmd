348f46640a2aeda15aa566f4a6d82a9303ebd1ad
Mastahac Bogdan <mastahac_bogdan@hotmail.com>
2021-03-02 16:38:43 +0200

    Add files via upload

---
+++ neta/network/kube916/attach.cmd
@@ -0,0 +1,20 @@
+@ECHO OFF
+SETLOCAL
+SET NUMBER_OF_NODES=4
+SET /A NODE_NUMBER=%1
+
+if "%1"=="" (
+    echo Please provide the number of the node to attach to (i.e. attach.cmd 2^) && EXIT /B 1
+)
+
+if %NODE_NUMBER% EQU 0 (
+    echo Please provide the number of the node to attach to (i.e. attach.cmd 2^) && EXIT /B 1
+)
+
+if %NODE_NUMBER% GEQ %NUMBER_OF_NODES%+1 (
+    echo %1 is not a valid node number. Must be between 1 and %NUMBER_OF_NODES%. && EXIT /B 1
+)
+
+FOR /f "delims=" %%g IN ('kubectl get pod --field-selector=status.phase^=Running -o name ^| findstr quorum-node%NODE_NUMBER%') DO set POD=%%g
+ECHO ON
+kubectl exec -it %POD% -c quorum -- /geth-helpers/geth-attach.sh
---
+++ neta/network/kube916/getEndpoints.cmd
@@ -0,0 +1,44 @@
+@ECHO OFF
+SETLOCAL
+SET NUMBER_OF_NODES=4
+SET /A NODE_NUMBER=%1
+
+if "%1"=="" (
+    echo Please provide the number of the node to attach to (i.e. attach.cmd 2^) && EXIT /B 1
+)
+
+if %NODE_NUMBER% EQU 0 (
+    echo Please provide the number of the node to attach to (i.e. attach.cmd 2^) && EXIT /B 1
+)
+
+if %NODE_NUMBER% GEQ %NUMBER_OF_NODES%+1 (
+    echo %1 is not a valid node number. Must be between 1 and %NUMBER_OF_NODES%. && EXIT /B 1
+)
+
+minikube ip >nul 2>&1
+if ERRORLEVEL 1 (
+  FOR /f "delims=" %%g IN ('minikube ip') DO set IP_ADDRESS=%%g
+) else (
+  set IP_ADDRESS=localhost
+)
+
+FOR /f "delims=" %%g IN ('minikube ip 2^>nul ^|^| echo localhost') DO set IP_ADDRESS=%%g
+
+FOR /F "tokens=* USEBACKQ" %%g IN (`kubectl get service quorum-node%NODE_NUMBER% -o^=jsonpath^="{range.spec.ports[?(@.name=='rpc-listener')]}{.nodePort}"`) DO set QUORUM_PORT=%%g
+
+FOR /F "tokens=* USEBACKQ" %%g IN (`kubectl get service quorum-node%NODE_NUMBER% -o^=jsonpath^="{range.spec.ports[?(@.name=='tm-tessera-third-part')]}{.nodePort}"`) DO set TESSERA_PORT=%%g
+
+echo quorum rpc: http://%IP_ADDRESS%:%QUORUM_PORT%
+echo tessera 3rd party: http://%IP_ADDRESS%:%TESSERA_PORT%
+
+kubectl get service cakeshop-service >nul 2>&1
+if ERRORLEVEL 1 (
+  FOR /F "tokens=* USEBACKQ" %%g IN (`kubectl get service cakeshop-service -o^=jsonpath^="{range.spec.ports[?(@.name=='http')]}{.nodePort}"`) DO set CAKESHOP_PORT=%%g
+  echo cakeshop: http://%IP_ADDRESS%:%CAKESHOP_PORT%
+)
+
+kubectl get service quorum-monitor >nul 2>&1
+if ERRORLEVEL 1 (
+  FOR /F "tokens=* USEBACKQ" %%g IN (`kubectl get service quorum-monitor -o^=jsonpath^="{range.spec.ports[?(@.name=='prometheus')]}{.nodePort}"`) DO set PROMETHEUS_PORT=%%g
+  echo prometheus: http://%IP_ADDRESS%:%PROMETHEUS_PORT%
+)
